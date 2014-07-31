5f2651c7c139b61f61d4b718c1958a2872ed5163
David Heien <david.heien@gmail.com>
2014-07-31 10:10:06 -0400

    customize routes, update 'cities' views, generate helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+	#Here, will have a master list of answers,and the user's quess will be checked against that list.
+
+	def check_answer(answer)
+
+		citylist = ["Atlanta", "Asheville", "Charlotte", "Raleigh"]
+
+		correct = false
+
+		citylist.each do |c|
+			if answer == c
+				correct = true
+			end
+		end
+		return correct
+	end
+
+	def check_for_dup(answer, database)
+
+		is_dup = false
+
+		database.each do |d|
+			if answer == d.name
+				is_dup = true
+			end
+		end
+		return is_dup
+	end
+	
+
 end
